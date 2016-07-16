c18bbcc3601e31f3a4db4bac83dcf3ecfaf441e4
Andrew Novikov <andruha2921@gmail.com>
2016-07-16 16:16:15 +0300

    add branch to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application,     'bookstore'
 set :user,            'bookstore'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # Don't change these unless you know what you're doing
 set :pty,             true
