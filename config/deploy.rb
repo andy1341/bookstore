bc75debe77adde3af4604e3697fa7d301212f0a6
Andrew Novikov <andruha2921@gmail.com>
2016-07-16 16:34:18 +0300

    check gem versions

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_p  ath}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
@@ -50,11 +50,11 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
+      # unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      #   puts "WARNING: HEAD is not the same as origin/master"
+      #   puts "Run `git push` to sync changes."
+      #   exit
+      # end
     end
   end
 
